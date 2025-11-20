acdc70edb56de62ac902d03c70e373e2c7b13e9e
Charles <carlosaraoz1999@gmail.com>
2025-11-20 07:35:40 -0300

    Mejorando seguridad con variables de entorno

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://psptaeipuxysqbuqbotq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzcHRhZWlwdXh5c3FidXFib3RxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NTk4MjcsImV4cCI6MjA3OTAzNTgyN30.6nV2c_IBDKdL13YN1IBQ11Kga4obLxHf8Z8fUKFWFhE'
+// En Vite, las variables se leen así: import.meta.env.NOMBRE_VARIABLE
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
